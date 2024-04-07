ac5a6533b498297b67aeabc4a88ef8bb4a3ec77b
AdithyaToms <141359083+AdithyaToms@users.noreply.github.com>
2024-04-08 03:44:54 +0530

    Update resizeandreorient.py

--- resizeandreorient.py
+++ resizeandreorient.py
@@ -1,41 +1,30 @@
 from pypdf import PdfReader, PdfWriter, Transformation
 from pypdf.generic import RectangleObject
+def to_specific_size_and_orientation(input_file, output_file, target_size, target_orientation):
+    with open(input_file, 'rb') as file:
+        pdf = PdfReader(file)
+        writer = PdfWriter()
+        for page_number in range(len(pdf.pages)):
+            page = pdf.pages[page_number]
+            height = float(page.mediabox.height)
+            width = float(page.mediabox.width)
+            if(target_orientation=="landscape" and width<height):
+                page.rotate(-90)
+            elif(target_orientation=="portrait" and width>height):
+                page.rotate(90)
+            target_width, target_height = get_page_dimensions(target_size,target_orientation)
+            r = RectangleObject([0, 0, target_width, target_height])
+            page.scale_by(min(target_width/width, target_height/height))
+            page.artbox = page.mediabox
+            page.cropbox = page.mediabox
+            page.bleedbox = page.mediabox
+            page.trimbox =  page.mediabox
+            page.transfer_rotation_to_content()
+            writer.add_page(page)
+        with open(output_file, 'wb') as out_file:
+            writer.write(out_file)
 
-def to_specific_size_and_orientation(input_file, output_file, target_size,target_orientation):
-    reader = PdfReader(input_file)
-    writer = PdfWriter()
-    target_width, target_height = get_page_dimensions(target_size)
-    r = RectangleObject([0, 0, target_width, target_height])
-
-    for page in reader.pages:
-        width = float(page.mediabox.width)
-        height = float(page.mediabox.height)
-        if(target_orientation=="landscape" and width<height):
-            page.rotate(-90)
-        elif(target_orientation=="portrait" and width>height):
-            page.rotate(90)
-        scale_by = min(target_height/height, target_width/width)
-
-        new_width = float(width * scale_by)
-        new_height = float(height * scale_by)
-
-        dx = (target_width - new_width) / 2
-        dy = (target_height - new_height) / 2
-        transform = Transformation().translate(tx=dx, ty=dy)
-
-        page.scale_to(width=new_width, height=new_height)
-        page.add_transformation(transform)
-        page.mediabox = r
-        page.artbox = r
-        page.cropbox = r
-        page.bleedbox = r
-        page.trimbox = r
-        writer.add_page(page)
-
-    with open(output_file, "wb"):
-        writer.write(output_file)
-
-def get_page_dimensions(page_size): 
+def get_page_dimensions(page_size,target_orientation): 
     sizes = { 'a0': (2384, 3370), 
              'a1': (1684, 2384), 
              'a2': (1191, 1684), 
@@ -48,7 +37,9 @@ def get_page_dimensions(page_size):
              'ledger': (791,1225), 
              'executive': (522,756), 
              } 
-    width, height = sizes[page_size.lower()] 
+    width, height = sizes[page_size.lower()]
+    if(target_orientation=="landscape"):
+        width, height = height, width
     return width, height 
 def main(): 
     input_files = [] 
