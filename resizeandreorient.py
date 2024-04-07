874bd080f9bd7f22d26db4540903988a0b31268a
AdithyaToms <141359083+AdithyaToms@users.noreply.github.com>
2024-04-08 02:22:58 +0530

    Update resizeandreorient.py

--- resizeandreorient.py
+++ resizeandreorient.py
@@ -1,24 +1,39 @@
-import PyPDF2
-from PyPDF2.generic import RectangleObject
+from pypdf import PdfReader, PdfWriter, Transformation
+from pypdf.generic import RectangleObject
 
-def to_specific_size_and_orientation(input_file, output_file, target_size, target_orientation):
-    with open(input_file, 'rb') as file:
-        pdf = PyPDF2.PdfReader(file)
-        writer = PyPDF2.PdfWriter()
-        for page_number in range(len(pdf.pages)):
-            page = pdf.pages[page_number]
-            height = float(page.mediabox.height)
-            width = float(page.mediabox.width)
-            if(target_orientation=="landscape" and width<height):
-                page.rotate(-90)
-            elif(target_orientation=="portrait" and width>height):
-                page.rotate(90)
-            target_width, target_height = get_page_dimensions(target_size)
-            page.scale_by(min(target_width/width, target_height/height))
-            page.transfer_rotation_to_content()
-            writer.add_page(page)
-        with open(output_file, 'wb') as out_file:
-            writer.write(out_file)
+def to_specific_size_and_orientation(input_file, output_file, target_size,target_orientation):
+    reader = PdfReader(input_file)
+    writer = PdfWriter()
+    target_width, target_height = get_page_dimensions(target_size)
+    r = RectangleObject([0, 0, target_width, target_height])
+
+    for page in reader.pages:
+        width = float(page.mediabox.width)
+        height = float(page.mediabox.height)
+        if(target_orientation=="landscape" and width<height):
+            page.rotate(-90)
+        elif(target_orientation=="portrait" and width>height):
+            page.rotate(90)
+        scale_by = min(target_height/height, target_width/width)
+
+        new_width = float(width * scale_by)
+        new_height = float(height * scale_by)
+
+        dx = (target_width - new_width) / 2
+        dy = (target_height - new_height) / 2
+        transform = Transformation().translate(tx=dx, ty=dy)
+
+        page.scale_to(width=new_width, height=new_height)
+        page.add_transformation(transform)
+        page.mediabox = r
+        page.artbox = r
+        page.cropbox = r
+        page.bleedbox = r
+        page.trimbox = r
+        writer.add_page(page)
+
+    with open(output_file, "wb"):
+        writer.write(output_file)
 
 def get_page_dimensions(page_size): 
     sizes = { 'a0': (2384, 3370), 
@@ -35,7 +50,6 @@ def get_page_dimensions(page_size):
              } 
     width, height = sizes[page_size.lower()] 
     return width, height 
-
 def main(): 
     input_files = [] 
     num_pdfs = int(input("Enter the number of PDF files: ")) 
